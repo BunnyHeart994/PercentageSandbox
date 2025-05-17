8aca355b5192a68f66387c7dc39dfb4983cdbbb0
unknown <bunnyheart94@gmail.com>
2025-05-17 18:57:04 -0300

    Work in progress

---
+++ include/descriptions.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include "definitions.h"
+
+void describe1() // PERCENTAGE EXTRACTOR
+{
+	PRINT_ENDL(std::endl << "Percentage extractor asks the user for a number then a percentage," << std::endl <<
+	"then shows how much of that number exists in that percentage.");
+}
+void describe2() // PERCENTAGE SUBTRACTOR
+{
+	PRINT_ENDL(std::endl << "Percentage subtractor asks the user for a number than a percentage," << std::endl <<
+	"then shows the remainder of that percentage subtracted from the number.");
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,113 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
 #include "../include/definitions.h"
+#include "../include/descriptions.h"
+//#define MENU_ICON '*'
 
-const USHORT COL_WIDTH_MENU = 98;
+// CONSTANTS
+const USHORT COL_WIDTH_MENU = 98; 
+const char MENU_ICON = '=';		    
 
-static void closeApp() { PRINT_ENDL(std::endl << "Bye." << std::endl); }
-static void getPercentageOf()
+// PROTOTYPED FUNCTIONS
+static void mainMenu(char defChar);
+USHORT descPrompt(void (*funcPtr)());
+
+static void closeApp() { PRINT_ENDL(std::endl << "Bye." << std::endl); } // One-line function, because it's small.
+static void extractPercentage() // 1
 {
-	float percentage, target;
-	PRINT_ENDL("*PERCENTAGE CALCULATOR*");
-	PRINT("Enter a percentage: ");
-	GET(percentage);
-	PRINT("Enter the target number: ");
-	GET(target);
-	//float result = target * (percentage / 100);
-	PRINT_ENDL(percentage << "% of " << target << " is " << target * (percentage / 100) << "." << std::endl);
-}
-static void minusPercentageOf()
-{
+	if (descPrompt(describe1) == 0) return;
+
 	float target, percentage;
 	PRINT_ENDL("*PERCENTAGE EXTRACTOR*");
-
+	PRINT("Enter the target number: ");
+	GET(target);
+	PRINT("Enter a percentage: ");
+	GET(percentage);
+	//float result = target * (percentage / 100);
+	PRINT_ENDL(percentage << "% of " << target << " is " << target * (percentage / 100) << "." << std::endl);
+	
+	//mainMenu(MENU_ICON); // PROBLEM -> DON'T DO THIS
 }
-static void mainMenu(char defChar)
+static void percentageSubtractor() // 2
 {
-	PRINT_ENDL(std::setfill(defChar) << std::setw(COL_WIDTH_MENU) << ""); // Draw upper boundary.
-	PRINT(std::setfill(' '));
+	if (descPrompt(describe2) == 0) return;
 
-	PRINT(std::left << std::setw(COL_WIDTH_MENU / 3) << "| 1 - Percentage calculator" << '|');
-	PRINT(std::left << std::setw(COL_WIDTH_MENU / 3) << "2 - Incognito" << '|');
-	PRINT_ENDL(std::right << std::setw((COL_WIDTH_MENU / 3) + COL_WIDTH_MENU % 3 - 2) << "3 - Incognito |");
-
-	PRINT_ENDL(std::setfill(defChar) << std::setw(COL_WIDTH_MENU) << ""); // Draw lower boundary.
-
-	USHORT opt;
-	PRINT("Enter an option: ");
+	float target, percentage;
+	PRINT_ENDL("*PERCENTAGE SUBTRACTOR*");
+	PRINT("Enter the target number: ");
+	GET(target);
+	PRINT("Enter a percentage (1 - 100): ");
+	GET(percentage);
+	while (percentage < 1 || percentage > 100)
+	{
+		PRINT("Invalid value. Only 1 through 100 is allowed." << std::endl << "Try again: ");
+		GET(percentage);
+	}
+	PRINT_ENDL(target << " minus " << percentage << "% is " << target - target * (percentage / 100) << ".");
+}
+USHORT descPrompt(void (*funcPtr)()) // DESCRIPTION PROMPT - FUNCTION POINTER
+{
+	USHORT choice;
+	//bool stop = false;
+	funcPtr();
+	PRINT_ENDL("Do you wish to continue?");
 	while (true)
 	{
+		PRINT("1 - Yes" << std::endl << "0 - Back to main menu" << std::endl <<
+			"---> ");
+		GET(choice);
+		switch (choice)
+		{
+		case 0: return 0; break;
+		case 1: return 1; break; //stop = true; break;
+		default: PRINT_ENDL("Invalid option. Try again."); break;
+		}
+		//break;
+	}
+}
+static void mainMenu(char defChar) // Main menu
+{
+	USHORT opt;
+	bool drawBox = true;
+	do
+	{
+		if (drawBox)
+		{
+			PRINT(std::setfill(' '));
+			PRINT_ENDL(std::setw(COL_WIDTH_MENU / 3) << "" << std::left << "* MAIN MENU *");
+			PRINT_ENDL(std::setfill(defChar) << std::setw(COL_WIDTH_MENU) << ""); // Draw upper boundary.
+			PRINT(std::setfill(' '));
+
+			PRINT(std::left << std::setw(COL_WIDTH_MENU / 3) << "| 1 - Percentage extractor" << '|');
+			PRINT(std::left << std::setw(COL_WIDTH_MENU / 3) << "2 - Percentage subtractor" << '|');
+			PRINT_ENDL(std::right << std::setw((COL_WIDTH_MENU / 3) + COL_WIDTH_MENU % 3 - 2) << "3 - Incognito |");
+
+			PRINT_ENDL(std::setfill(defChar) << std::setw(COL_WIDTH_MENU) << ""); // Draw lower boundary.
+		}
+		
+		drawBox = true;
+		PRINT("Enter an option from the box above: ");
 		GET(opt);
 		switch (opt)
 		{
+		case 0: break;
 		case 1:
-			getPercentageOf(); break;
-		case 0:
-			closeApp(); break;
+			extractPercentage(); break;
+		case 2:
+			percentageSubtractor(); break;
 		default:
-			PRINT("Invalid option. Try again." << std::endl << "Enter an option: "); continue;
+			PRINT("Invalid option. Try again." << std::endl); drawBox = false; break;
 		}
-		break;
-	}
+	} while (opt != 0);
+	closeApp();
+	return;
 }
 int main()
 {
 	PRINT_ENDL("");
 	//PRINT_ENDL("*************************************");
 	PRINT_ENDL("Percentage Sandbox by Henrique Soares" << std::endl);
-	mainMenu('*');
-
+	mainMenu(MENU_ICON);
 	//PRINT_ENDL(std::endl << "*************************************" << std::endl);
+	return 0;
 }
